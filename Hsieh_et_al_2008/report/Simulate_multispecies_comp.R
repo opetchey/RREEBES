cb8089d9c59517cc509e74df8dbcdf8cc1825e39
Frank Pennekamp <pennekampster@gmail.com>
2016-04-01 14:52:29 +0200

    Some cleaning of the script for multi-species model before emailing C.Hsieh

--- Hsieh_et_al_2008/report/Simulate_multispecies_comp.R
+++ Hsieh_et_al_2008/report/Simulate_multispecies_comp.R
@@ -6,22 +6,13 @@ set.seed()
 
 ## Specify the number of species
 S <- 5
-alpha <- 0.005
 
 #growth rate in the eight-mode oscillation regime below the threshold to chaos
 r <- rep(3.57, S)
 
-# play around with growth rates to understand behaviour and steady state
-#r<- runif(5, 1, 2)
-
-# add some process noise on the interaction coefficient
-#a <- matrix(runif(S^2, 0.85, 1.15), nrow=S, ncol=S)*alpha
-#a <- matrix(alpha, nrow=S, ncol=S)
+# specify alphas
+alpha <- 0.05
 a <- rep(alpha,S)
-
-
-# set diagonal elements to 1
-#diag(a) <- NA
 
 # simulate 1000 time steps
 t=seq(1,1000, by=1)
@@ -32,15 +23,14 @@ N <- matrix(nrow=S, ncol=length(t))
 # set initial conditions randomly
 N[,1] <- runif(S)
 
-# simulate dynamics using the standard logistic map
-
+# simulate dynamics using the discrete logistic growth model; add process noise at the end
 for (i in 1:(length(t)-1)) {
  
-  N[,i+1] = (r * N[,i] * (1- N[,i] - a*N[,i])) * runif(1,0.95,1.05)
+  N[,i+1] = (r * N[,i] * (1- N[,i] - a*N[,i])) * runif(1,0.85,1.15)
   
 }
 
-# add observation noise
+# add some observation noise to time series
 N <- N * runif(length(N), 0.85, 1.15)
 
 # plot results
@@ -50,5 +40,8 @@ matplot(t,t(N[1:S,]), "l",ylim=c(0,1), xlim=c(0,1000),col=2:5)
 output <- as.data.frame(t(N[1:S,]))
 names(output) <- paste0("species ", 1:S)
 
-write.csv(output, "/Users/Frank/Documents/Git projects/RREEBES/Hsieh_et_al_2008/data/5sp_comp_sim_data.csv", row.names=F)
+# check for correlation in abundances
+plot(output$`species 1`, output$`species 5`)
+
+#write.csv(output, "/Users/Frank/Documents/Git projects/RREEBES/Hsieh_et_al_2008/data/5sp_comp_sim_data.csv", row.names=F)
 
