283a8ec5e78ae0a55f5a6b0c2113291a2136ec9e
Frank Pennekamp <pennekampster@gmail.com>
2016-04-01 14:45:24 +0200

    Some work on the 5 species competition model, but still not finished. Need to contact C. Hsieh

--- Hsieh_et_al_2008/report/Simulate_multispecies_comp.R
+++ Hsieh_et_al_2008/report/Simulate_multispecies_comp.R
@@ -2,11 +2,11 @@
 # the equilibrium density depends on the growth rate (steady state at values between 1 and 2, extinction below 1, chaos > 3.7, fluctuations > 3)
 
 # set seed
-set.seed(1352147634)
+set.seed()
 
 ## Specify the number of species
 S <- 5
-alpha <- 0.01
+alpha <- 0.005
 
 #growth rate in the eight-mode oscillation regime below the threshold to chaos
 r <- rep(3.57, S)
@@ -14,11 +14,14 @@ r <- rep(3.57, S)
 # play around with growth rates to understand behaviour and steady state
 #r<- runif(5, 1, 2)
 
-# add some process noise on the interaction coefficients
-a <- matrix(runif(S^2, 0.85, 1.15), nrow=S, ncol=S)*alpha
+# add some process noise on the interaction coefficient
+#a <- matrix(runif(S^2, 0.85, 1.15), nrow=S, ncol=S)*alpha
+#a <- matrix(alpha, nrow=S, ncol=S)
+a <- rep(alpha,S)
+
 
 # set diagonal elements to 1
-diag(a) <- 1
+#diag(a) <- NA
 
 # simulate 1000 time steps
 t=seq(1,1000, by=1)
@@ -30,13 +33,22 @@ N <- matrix(nrow=S, ncol=length(t))
 N[,1] <- runif(S)
 
 # simulate dynamics using the standard logistic map
-for (i in 1:(length(t)-1))  N[,i+1] = (r * N[,i] * (1- a%*%N[,i]))*runif(1,0.95,1.05)
+
+for (i in 1:(length(t)-1)) {
+ 
+  N[,i+1] = (r * N[,i] * (1- N[,i] - a*N[,i])) * runif(1,0.95,1.05)
+  
+}
+
+# add observation noise
+N <- N * runif(length(N), 0.85, 1.15)
 
 # plot results
-matplot(t,t(N[1:S,]), "l",ylim=c(0,1),col=2:5)
+matplot(t,t(N[1:S,]), "l",ylim=c(0,1), xlim=c(0,1000),col=2:5)
+
 
 output <- as.data.frame(t(N[1:S,]))
 names(output) <- paste0("species ", 1:S)
 
-write.csv(output, "/Users/Frank/Documents/Github projects/RREEBES/Hsieh_et_al_2008/data/5sp_comp_sim_data.csv", row.names=F)
+write.csv(output, "/Users/Frank/Documents/Git projects/RREEBES/Hsieh_et_al_2008/data/5sp_comp_sim_data.csv", row.names=F)
 
