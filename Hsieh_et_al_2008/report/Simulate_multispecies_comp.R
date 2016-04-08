5ebb915b82b5b71b5f7e605cb1360f5c93819719
Frank Pennekamp <pennekampster@gmail.com>
2016-04-08 16:22:38 +0200

    Fixed bug in simulation model according to suggestions by C. Anderson

--- Hsieh_et_al_2008/report/Simulate_multispecies_comp.R
+++ Hsieh_et_al_2008/report/Simulate_multispecies_comp.R
@@ -2,17 +2,16 @@
 # the equilibrium density depends on the growth rate (steady state at values between 1 and 2, extinction below 1, chaos > 3.7, fluctuations > 3)
 
 # set seed
-set.seed()
+set.seed(190981)
 
 ## Specify the number of species
 S <- 5
 
 #growth rate in the eight-mode oscillation regime below the threshold to chaos
-r <- rep(3.57, S)
+r <- 3.57
 
 # specify alphas
-alpha <- 0.05
-a <- rep(alpha,S)
+a <- 0.05
 
 # simulate 1000 time steps
 t=seq(1,1000, by=1)
@@ -24,14 +23,16 @@ N <- matrix(nrow=S, ncol=length(t))
 N[,1] <- runif(S)
 
 # simulate dynamics using the discrete logistic growth model; add process noise at the end
-for (i in 1:(length(t)-1)) {
- 
-  N[,i+1] = (r * N[,i] * (1- N[,i] - a*N[,i])) * runif(1,0.85,1.15)
+for (i in 1:(length(t)-1)){
   
+  for (j in 1:5){
+
+    N[j,i+1] = (r * N[j,i] * (1- N[j,i]) - a*sum(N[-j,i]) ) * runif(1,0.85,1.15)
+  }
 }
 
 # add some observation noise to time series
-N <- N * runif(length(N), 0.85, 1.15)
+N <- N * runif(length(N), 0.95, 1.05)
 
 # plot results
 matplot(t,t(N[1:S,]), "l",ylim=c(0,1), xlim=c(0,1000),col=2:5)
@@ -43,5 +44,5 @@ names(output) <- paste0("species ", 1:S)
 # check for correlation in abundances
 plot(output$`species 1`, output$`species 5`)
 
-#write.csv(output, "/Users/Frank/Documents/Git projects/RREEBES/Hsieh_et_al_2008/data/5sp_comp_sim_data.csv", row.names=F)
+write.csv(output, "/Users/Frank/Documents/Github projects/RREEBES/Hsieh_et_al_2008/data/5sp_comp_sim_data.csv", row.names=F)
 
